04fcfc82f606e421b8f846bb33564269bb460de2
Matheus Castro <MATHEUSCASTRO161096@gmail.com>
2022-07-13 12:35:42 -0300

    Working in time interval.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#play').addEventListener('click', function () {
+        var value = document.querySelector('#time').value
+        if (value != null && value != '') {
+            setTimeout(() => {
+                alert("test")
+            }, value * 1000);
+        }
+        else
+        alert("informe um tempo para o alerta")
+    });
 
 });
